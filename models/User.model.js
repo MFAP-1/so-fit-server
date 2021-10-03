f629355a02a117c93760c5a515c96babd320dfb5
Manoel Pereira <manoelpereiraeng@gmail.com>
2021-10-03 20:42:58 -0300

    workout duplicate helper route. User level up implemented.

--- models/User.model.js
+++ models/User.model.js
@@ -18,7 +18,7 @@ const UserSchema = new Schema({
     type: String,
     trim: true,
     maxlength: 500,
-    required:true,
+    required: true,
     default: "Sem descrição",
   },
   passwordHash: { type: String, required: true },
@@ -36,16 +36,33 @@ const UserSchema = new Schema({
   },
   soFitPoints: { type: Number, min: 0, default: 0 },
   level: { type: Number, min: 1, default: 1 },
-  followingId:[{type: Types.ObjectId, ref:"User"}], //Precisa ser required?
-  followersId :[{type: Types.ObjectId, ref:"User"}] //Precisa ser required?
+  followingId: [{ type: Types.ObjectId, ref: "User" }], //Precisa ser required?
+  followersId: [{ type: Types.ObjectId, ref: "User" }], //Precisa ser required?
 });
-
 
 UserSchema.pre("save", function (next) {
   this.name =
     this.name.trim()[0].toUpperCase() + this.name.slice(1).toLowerCase();
- 
+
   next();
+});
+
+// Updating the level. Checking at every change in the soFiPoints value
+function determinateLevelBasedOnPoints(amountOfPoints) {
+  const experienceTable = [1000, 2050, 4200, 8600, 17600, 36080, 73000]; // pre-defined required amount of points for leveling up
+  let level = 1;
+  for (let i = 0; i < experienceTable.length; i++) {
+    console.log(amountOfPoints, "pontos no for loop");
+    if (amountOfPoints > experienceTable[i]) {
+      level = i + 2; // +2 cause in the 0 index is the level up from lvl 1 to 2. And so on.
+    }
+  }
+  return level;
+}
+
+UserSchema.post("findOneAndUpdate", function (result) {
+  result.level = determinateLevelBasedOnPoints(result.soFitPoints);
+  result.save();
 });
 
 const UserModel = model("User", UserSchema);
